b508cd21b8eb16713f04b0bb2e737d10209c9250
Sedanur97 <80327814+Sedanur97@users.noreply.github.com>
2021-11-02 23:41:08 +0100

    Update 4g.js

--- LAB4/4g.js
+++ LAB4/4g.js
@@ -5,28 +5,30 @@ function typeOF(object) {
 
 var numbers = [1,2,3,4,5];
 console.log(numbers);
+console.log(typeof numbers);
 
 var person = {
     name: "Seda Nur", 
     age: 22
 }
+console.log(person);
+console.log(typeof person);
 
-console.log(type(null));
+var hello = function (){
+    console.log("Hello");
+}
+
+console.log(hello);
+console.log(typeof hello);
+console.log(typeOF("seda"));
+
+console.log(typeOF(3.1));
+console.log(typeOF(8));
+console.log(typeOF(null));
+
 
 var date = new Date();
 console.log(date);
 console.log(typeof date);
 
-var hello = function (){
-    console.log("Hello");
-}
-console.log(hello);
-console.log(typeof hello);
-console.log(type("seda"));
 
-console.log(person);
-console.log(typeof person);
-
-console.log(typeof numbers);
-console.log(type(3.14));
-console.log(type(8));
