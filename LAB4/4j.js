54bd697a23712befd77d9f3180b8359fed5b90bd
Sedanur97 <80327814+Sedanur97@users.noreply.github.com>
2021-11-03 00:09:36 +0100

    Create 4j.js

---
+++ LAB4/4j.js
@@ -0,0 +1,20 @@
+//Write a JavaScript program for binary search
+
+function binarySearch(ar, num) {
+    var m = 0;
+    var n = ar.length - 1;
+    while (m <= n) {
+        var k = (n + m) >> 1;
+        var cmp = compare_fn(num, ar[k]);
+        if (cmp > 0) {
+            m = k + 1;
+        } else if(cmp < 0) {
+            n = k - 1;
+        } else {
+            return k;
+        }
+    }
+    return -m - 1;
+}
+var result = binarySearch(3, 100);
+console.log(result);
