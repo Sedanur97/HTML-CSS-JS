f37df220ce6949408f8ff4ec912fe807569d2f6e
Sedanur <80327814+Sedanur97@users.noreply.github.com>
2021-11-14 22:53:43 +0100

    Delete Lab06/6a/6b directory

--- Lab06/6a/6b/6b.js
+++
@@ -1,9 +0,0 @@
-setTimeout(appendElement, 5000);
-
-function appendElement() {
-    var paragraf = document.createElement("p");
-    var text = document.createTextNode("b)	Write a JS function that will add a paragraph of text to the page after 5 seconds from opening the page");
-    paragraf.appendChild(text);
-    var body = document.querySelector("body");
-    body.appendChild(paragraf);
-}
