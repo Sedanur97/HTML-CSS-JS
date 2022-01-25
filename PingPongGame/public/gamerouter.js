0071733329b5d7ac3b710aa1f4a260917b4d5dce
Sedanur <80327814+Sedanur97@users.noreply.github.com>
2022-01-25 17:05:52 +0100

    Add files via upload

---
+++ PingPongGame/public/gamerouter.js
@@ -0,0 +1,8 @@
+const path = require('path')
+
+module.exports.game = function (req, res) {
+  // player.play('../sounds/stadium.mp3', function (err) {
+  //   if (err) throw err
+  // })
+  res.sendFile(path.join(__dirname, '../index.html'))
+}
